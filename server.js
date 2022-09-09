7d2ebde2f4809f12b37dcb4e3676eba80bc01fca
Brad Culley <culleyib@gmail.com>
2022-09-09 12:21:54 -0400

    Adding more example uses

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const client = new Discojs.Discojs({
     userToken: process.env.USER_TOKEN,
 })
 
+// search among releases
 client
     .searchRelease('Damon Albarn - Everyday Robots', {
         releaseTitle: 'Damon Albarn - Everyday Robots'
@@ -13,3 +14,19 @@ client
         console.log(results)
     });
 
+// search among releases
+client
+    .searchDatabase({
+        query: 'Damon Albarn - Everyday Robots',
+        type: "release"
+    })
+    .then(({results}) => {
+        console.log(results)
+    });
+
+// get single release
+client
+    .getRelease(5636870)
+    .then((results) => {
+        console.log(results)
+    });
